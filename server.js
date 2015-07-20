6bcf3b42c030d1e2258d333eb548fe80a505b1a0
kanasugara <mriynuk@hotmail.com>
2015-07-20 15:10:12 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 8080;
+
+var cookieParser = require("cookie-parser");
+var session = require("express-session");
+var morgan = require("morgan");
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(session({secret:'anystringoftext',
+                saveUninitialized: true,
+                resave: true}));
+
+
+app.use('/', function(req, res){
+  res.send('Stop starring at my cock');
+  console.log(req.cookies);
+  console.log('================================');
+  console.log(req.session);
+});
+
+app.listen(port);
+console.log('Sever running on ' + port);
