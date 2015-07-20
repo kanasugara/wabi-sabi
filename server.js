f00813956aeac674f50629ad8eae0700e5e068bf
kanasugara <mriynuk@hotmail.com>
2015-07-20 16:18:17 +0000

    third commit

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
-var express = require("express");
+var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 
-var cookieParser = require("cookie-parser");
-var session = require("express-session");
-var morgan = require("morgan");
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+
+var configDB = require('./config/database.js');
+mongoose.connect(configDB.url);
 
 app.use(morgan('dev'));
 app.use(cookieParser());
-app.use(session({secret:'anystringoftext',
-                saveUninitialized: true,
-                resave: true}));
+app.use(session({secret: 'anystringoftext',
+				 saveUninitialized: true,
+				 resave: true}));
 
 
-app.use('/', function(req, res){
-  res.send('Stop starring at my cock');
-  console.log(req.cookies);
-  console.log('================================');
-  console.log(req.session);
-});
+// app.use('/', function(req, res){
+// 	res.send('Our First Express program!');
+// 	console.log(req.cookies);
+// 	console.log('================');
+// 	console.log(req.session);
+// });
+
+require('./app/routes.js')(app);
 
 app.listen(port);
-console.log('Sever running on ' + port);
+console.log('Server running on port: ' + port);
