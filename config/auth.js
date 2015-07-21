29856413809d251ed90f0f8c4e24b45fea08616a
kanasugara <mriynuk@hotmail.com>
2015-07-21 15:33:22 +0000

    facebook permition blocker

---
+++ config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'facebookAuth' : {
+        "clientID": "1610942912499426",
+        "clientSecret": "74a92af267e0f6f0af42280a241cce9d",
+        "callbackURL": "http://localhost:8080/auth/facebook/callback"
+    }
+};
